455d3e9539e0f149156fef6ceeac021a4ada3c03
RainmakerRaw <RainmakerRaw@users.noreply.github.com>
2018-12-13 21:28:59 +0000

    Update wg-up.sh

--- wg-up.sh
+++ wg-up.sh
@@ -3,6 +3,6 @@ ip link add dev azirevpn-uk1 type wireguard
 ip address add dev azirevpn-uk1 10.20.xx.xx/19
 wg setconf azirevpn-uk1 /etc/wireguard/azirevpn-uk1.conf
 ip link set up dev azirevpn-uk1
-ip rule add unicast iif [WAN interface, eg enp3s0f0] table vpn
+ip rule add unicast iif [WAN interface] table vpn
 ip route add default dev azirevpn-uk1 via 10.xx.xx.xx table vpn
-ip route add 192.168.2.0/24 via 192.168.2.1 dev [LAN interface, eg enp3s0f1] table vpn
+ip route add 192.168.2.0/24 via 192.168.2.1 dev [LAN interface] table vpn
